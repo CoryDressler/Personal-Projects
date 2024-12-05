67a1eb6c2d767cc156797e8a3863913f1a856ee0
CoryDressler <dressler.cory923@gmail.com>
2024-12-05 12:22:06 -0500

    Create Fraction.h

---
+++ Fraction/Fraction.h
@@ -0,0 +1,21 @@
+class Fraction{
+ private:
+  int numerator;
+  int denominator;
+
+public:
+   //Fraction();
+   Fraction(int num, int denom);
+   Fraction(const Fraction& x);
+   int getNumerator() const;
+   int getDenominator() const;
+   void setNumerator(int num);
+   void setDenominator(int denom);
+   Fraction operator+(Fraction const &x);
+   Fraction operator-(Fraction const &x);
+   Fraction operator*(Fraction const &x);
+   Fraction operator/(Fraction const &x);
+   void print() const;
+   void simplify();
+   
+};
